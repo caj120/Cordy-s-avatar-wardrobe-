7f5f99d41689a4ccc249f4ae9e54da4d57886b7a
caj120 <56417046+caj120@users.noreply.github.com>
2026-06-13 07:18:52 -0400

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const CLIENT_ID    = "flowsta_app_d73a9db182e5454a5f28a67523a7ae3d1e3cb8676db56ca0910e0b679b71cbb5";
+export const REDIRECT_URI = window.location.origin + "/auth/callback";
+export const AUTH_URL     = "https://auth-api.flowsta.com/oauth/authorize";
+export const TOKEN_URL    = "https://auth-api.flowsta.com/oauth/token";
+export const USERINFO_URL = "https://auth-api.flowsta.com/oauth/userinfo";
+export const REVOKE_URL   = "https://auth-api.flowsta.com/oauth/revoke";
+export const SCOPES       = "openid display_name username email profile_picture did public_key";
+export const WARDROBE_SERVER = "http://localhost:3000";
